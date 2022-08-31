c975cceb57fbc77648eb6f43703ba07c7e761c02
gunny6026 <gunny6026@naver.com>
2022-08-31 15:47:46 +0900

    feat : 데이터에 타입이 있을 때 장점
    데이터에 타입이 있으면 에러를 방지 할 수도 있고, 데이터 안에 속성이
    무엇인지 브라우저에서 확인 하지 않아도 된다.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 타입스크립트
 
+
+- [강의  교안](https://joshua1988.github.io/ts/)
--- why-ts/app.js
+++ why-ts/app.js
@@ -2,19 +2,21 @@
 var url = "https://jsonplaceholder.typicode.com/users";
 
 // dom
-var username = document.querySelector("#usename");
+var username = document.querySelector("#username");
 var email = document.querySelector("#email");
 var address = document.querySelector("#address");
 
 // user data
 var user = {};
-
 function startApp() {
   axios
     .get(url)
     .then(function (response) {
       console.log(response);
       user = response.data;
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
     })
     .catch(function (error) {
       console.log("error = ", error);
